11792e55fae90702358d0920a2e074567dcd7a8c
Renaldo Freire <orenaldo@proton.me>
2024-12-09 12:49:17 -0300

    rename folder script complete

---
+++ rename-folders.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+def rename_folders(path_dir):
+    for folder in os.listdir(path_dir):
+        folder_full_path = os.path.join(path_dir, folder)
+        
+        if os.path.isdir(folder_full_path):
+            folder_style = r'\((\d{4})\)\s*(.+?)\s*\[.+\]'
+            
+            corresp = re.match(folder_style, folder)
+            
+            if corresp:
+                year = corresp.group(1)
+                album_name = corresp.group(2).split('-')[-1].strip()
+                
+                new_name = f"{year} - {album_name}"
+                
+                new_path = os.path.join(path_dir, new_name)
+                
+                os.rename(folder_full_path, new_path)
+                print(f"Renomeado: {folder} -> {new_name}")
+
+# to use
+path_dir = '/mnt/disk1/Music/Name/'
+rename_folders(path_dir)
